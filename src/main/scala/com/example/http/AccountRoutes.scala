3a6ceabd14ff339054256d240ae9aaf519b9c75d
Bert Meeuws <bert.meeuws@inthepocket.com>
2022-06-27 21:24:32 +0200

    chore: changing directives to tildes

--- src/main/scala/com/example/http/AccountRoutes.scala
+++ src/main/scala/com/example/http/AccountRoutes.scala
@@ -43,90 +43,32 @@ class AccountRoutes(accountRegistry: ActorRef[AccountActor.Command])(implicit
 
   val accountRoutes: Route =
     pathPrefix("account") {
-      concat(
-        path("id") {
-          post {
-            entity(as[Multipart.FormData]) { formData =>
-              implicit val ec: scala.concurrent.ExecutionContext =
-                scala.concurrent.ExecutionContext.global
-
-              println(formData)
-
-              println(formData.getMediaType())
-
-              // collect all parts of the multipart as it arrives into a map
-              val allPartsF: Future[Map[String, Any]] = formData.parts
-                .mapAsync[(String, Any)](1) {
-
-                  case b: Multipart.BodyPart if b.name == "file" =>
-                    println("inside bodypart1")
-                    // stream into a file as the chunks of it arrives and return a future
-                    // file to where it got stored
-                    val file = File.createTempFile("upload", "tmp")
-                    b.entity.dataBytes
-                      .runWith(FileIO.toPath(file.toPath))
-                      .map(_ => (b.name -> file))
-
-                  case b: Multipart.FormData.BodyPart =>
-                    println("inside bodypart2")
-                    // collect form field values
-                    b.toStrict(new FiniteDuration(2, SECONDS))
-                      .map(strict => (b.name -> strict.entity.data.utf8String))
-
+      path(IntNumber) { int =>
+        post {
+          println("Posting")
+          complete(if (int % 2 == 0) "even ball" else "odd ball")
+        } ~
+          get {
+            println("inside get")
+            complete("Inisde getterr")
+          }
+      } ~
+        post {
+          println("insidepost")
+          entity(as[Account]) { account =>
+            {
+              println("succes??")
+              println(account)
+              onSuccess(createAccount(account)) { performed =>
+                {
+                  println("created")
+                  complete("Great success")
                 }
-                .runFold(Map.empty[String, Any])((map, tuple) => map + tuple)
 
-              val done1 = Await.result(allPartsF, Duration(5, "seconds"))
-
-              println(done1)
-
-              val done = allPartsF.map { allParts =>
-                // You would have some better validation/unmarshalling here
-                /*
-        db.create(Video(
-          file = allParts("file").asInstanceOf[File],
-          title = allParts("title").asInstanceOf[String],
-          author = allParts("author").asInstanceOf[String]))
-                 */
-              }
-
-              complete("Done")
-
-              // when processing have finished create a response for the user
-              onSuccess(allPartsF) { allParts =>
-                complete {
-                  "ok!"
-                }
               }
             }
           }
-        },
-        concat(
-          pathEnd(
-            concat(
-              get {
-                println("getting accounts")
-                complete("Getting accounts")
-              },
-              post {
-                println("insidepost")
-                entity(as[Account]) { account =>
-                  {
-                    println("succes??")
-                    println(account)
-                    onSuccess(createAccount(account)) { performed =>
-                      {
-                        println("created")
-                        complete("Great success")
-                      }
+        }
 
-                    }
-                  }
-                }
-              }
-            )
-          )
-        )
-      )
     }
 }
